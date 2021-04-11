08fef5538267987287f2192b05f2100b76597f2c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 21:29:30 +0400

    Saving contents

--- lib.c
+++ lib.c
@@ -33,10 +33,11 @@ bool register_user(char *username) {
 bool follow_user(char *username, char *who_to_follow) {
     for (int i = 0; i < MAX_USERS && i < max_index; ++i) {
         if (strcmp(registered_users[i], username) == 0) {
-            int this_folowee = followees[i] + 1;
-            follows[i] = realloc(follows[i], sizeof(this_folowee));
-            follows[i][this_folowee] = calloc(strlen(who_to_follow), sizeof(char));
-            strcpy(follows[i][this_folowee], who_to_follow);
+            int this_followee = followees[i] + 1;
+            follows[i] = realloc(follows[i], sizeof(this_followee));
+            follows[i][this_followee] = calloc(strlen(who_to_follow), sizeof(char));
+            strcpy(follows[i][this_followee], who_to_follow);
+            followees[i]++;
             return true;
         }
     }
