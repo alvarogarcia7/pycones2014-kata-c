834d7d015483ee9450f6765c790d048fb7a2b89a
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 21:34:00 +0400

    Saving contents

--- tests/test.c
+++ tests/test.c
@@ -34,12 +34,26 @@ static void follow_another_user(void **state) {
     assert_null(followees[1]);
 }
 
+
+static void follow_multiple_users(void **state) {
+    register_clear();
+    register_user("pepe");
+    assert_int_equal(true, follow_user("pepe", "juan"));
+    assert_int_equal(true, follow_user("pepe", "jose"));
+
+    char **followees = user_is_following("pepe");
+    assert_string_equal(followees[0], "juan");
+    assert_string_equal(followees[1], "jose");
+    assert_null(followees[2]);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(canary_test)
             , cmocka_unit_test(register_non_existing_username)
             , cmocka_unit_test(register_existing_username)
             , cmocka_unit_test(follow_another_user)
+            , cmocka_unit_test(follow_multiple_users)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
