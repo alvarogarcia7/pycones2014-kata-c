bf9ab75c66e24d0e2efe33b4d5774fe58f597158
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 20:56:27 +0400

    Saving contents

--- THOUGHTS.md
+++ THOUGHTS.md
@@ -2,3 +2,4 @@ Initialized gitignore
 
 Completed cmocka test setup from https://github.com/alvarogarcia7/c-simple-sessions/blob/master/cmocka_test/tests/CMakeLists.txt
 
+Executed first test 
--- tests/test.c
+++ tests/test.c
@@ -1,1 +1,19 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "../lib.h"
+
+/* A test case that does nothing and succeeds. */
+static void canary_test(void **state) {
+    (void) state; /* unused */
+    assert_int_equal(0, 0);
+}
+int main(void) {
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(canary_test)
+    };
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
 
