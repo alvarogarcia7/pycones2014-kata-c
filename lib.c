6dae2c3b16b2b84ff47c5e50be382c535b2b3dee
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 21:06:28 +0400

    Saving contents

--- lib.c
+++ lib.c
@@ -6,15 +6,17 @@
 #define MAX_USERS 100
 
 char *(registered_users)[MAX_USERS];
+int max_index =0;
 
 bool register_user(char *username) {
     int i = 0;
-    for (i = 0; i < MAX_USERS; ++i) {
+    for (i = 0; i < MAX_USERS && i<max_index; ++i) {
         if (strcmp(registered_users[i], username) == 0) {
             return false;
         }
     }
-//    registered_users[i] = calloc(strlen(username) + 1, sizeof(char));
-//    strcpy(registered_users[i], username);
+    registered_users[i] = calloc(strlen(username) + 1, sizeof(char));
+    strcpy(registered_users[i], username);
+    max_index++;
     return true;
 }
