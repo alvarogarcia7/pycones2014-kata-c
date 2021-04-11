04939f683eeae3ea1f6dce7b8cf01d278aed3c3c
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 21:03:58 +0400

    Saving contents

--- lib.c
+++ lib.c
@@ -1,7 +1,18 @@
 #include "lib.h"
 #include <stdbool.h>
+#include <string.h>
 
+#define MAX_USERS 100
+
+char *(registered_users)[MAX_USERS];
 
 bool register_user(char *username) {
+    int i = 0;
+    for (i = 0; i < MAX_USERS; ++i) {
+        if (strcmp(registered_users[i], username) == 0) {
+            return false;
+        }
+    }
+    strcpy(registered_users[i], username, strlen(username));
     return true;
 }
