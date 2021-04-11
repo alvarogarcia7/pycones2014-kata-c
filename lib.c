545fd91b1094c575d68a41526ce6bdd010e3bf93
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 22:54:21 +0400

    Saving contents

--- lib.c
+++ lib.c
@@ -84,7 +84,7 @@ char **export_contents() {
         lines[i] = calloc(line_size, sizeof(char));
 
         strcpy(lines[i], registered_users[i]);
-        strcpy((*(lines+i)[strlen(lines[i]]), ": ");
+        strcpy(&*(*(lines+i)+strlen(lines[i])), ": ");
     }
 
     return lines;
