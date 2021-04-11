d3fb617d38fc5f012c711bfd88335ca62299746f
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 20:54:57 +0400

    Saving contents

---
+++ lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alvaro garcia on 11/04/2021.
+//
+
+#include "lib.h"
---
+++ lib.h
@@ -0,0 +1,8 @@
+//
+// Created by alvaro garcia on 11/04/2021.
+//
+
+#ifndef PYCONES2014_KATA_LIB_H
+#define PYCONES2014_KATA_LIB_H
+
+#endif //PYCONES2014_KATA_LIB_H
