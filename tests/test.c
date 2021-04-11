d81c3927f530bda8daaa6404dfdc65ff57bf038b
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-11 21:00:58 +0400

    Saving contents

--- tests/test.c
+++ tests/test.c
@@ -17,10 +17,16 @@ static void register_non_existing_username(void **state) {
     assert_int_equal(true, register_user("pepe"));
 }
 
+static void register_existing_username(void **state) {
+    assert_int_equal(true, register_user("pepe"));
+    assert_int_equal(false, register_user("pepe"));
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(canary_test)
             , cmocka_unit_test(register_non_existing_username)
+            , cmocka_unit_test(register_existing_username)
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
