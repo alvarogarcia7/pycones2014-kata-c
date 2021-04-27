16d2774460844f104c836010b8f2898a350acfb9
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-27 20:53:17 +0400

    Saving contents

--- main.c
+++ main.c
@@ -8,16 +8,14 @@ int import_from_file(const char *FILENAME) {
     size_t line_buf_size = 0;
     ssize_t line_size;
     FILE *fp = fopen(FILENAME, "r");
-    if (!fp)
-    {
+    if (!fp) {
         fprintf(stderr, "Error opening file '%s'\n", FILENAME);
         return EXIT_FAILURE;
     }
 
     line_size = getline(&line_buf, &line_buf_size, fp);
 
-    while (line_size >= 0)
-    {
+    while (line_size >= 0) {
         import_contents(line_buf);
         line_size = getline(&line_buf, &line_buf_size, fp);
     }
@@ -31,8 +29,7 @@ int import_from_file(const char *FILENAME) {
 
 int export_to_file(const char *FILENAME) {
     FILE *fp = fopen(FILENAME, "w");
-    if (!fp)
-    {
+    if (!fp) {
         fprintf(stderr, "Error opening file '%s'\n", FILENAME);
         return EXIT_FAILURE;
     }
@@ -40,8 +37,7 @@ int export_to_file(const char *FILENAME) {
     char **results = export_contents();
 
     int i = 0;
-    while (results[i] != NULL)
-    {
+    while (results[i] != NULL) {
         fprintf(fp, "%s", results[i]);
         i++;
     }
@@ -53,13 +49,16 @@ int export_to_file(const char *FILENAME) {
 int main(int argc, char **argv) {
     bool some_command_found = true;
     import_from_file("../export.db");
-    if (argc >= 4 && strcmp(argv[2], "f") == 0 ){
+    if (argc >= 4 && strcmp(argv[2], "f") == 0) {
         follow_user(argv[1], argv[3]);
-    }
-    else {
+    } else if (strcmp(argv[1], "-h") == 0) {
+        printf("Usage: %s [-h] user [command command_parameters] \n", argv[0]);
+        printf("-h: Show this help\n");
+        printf("f: Follow. Usage 'f $WHO_TO_FOLLOW' \n");
+    } else {
         some_command_found = false;
     }
-    if(some_command_found) {
+    if (some_command_found) {
         export_to_file("../export.db");
     }
     return 0;
