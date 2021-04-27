23512da6996b2a67f7e5dd45b6cbe1197d0c3085
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-27 20:38:46 +0400

    Saving contents

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 int import_from_file(const char *FILENAME) {
     char *line_buf = NULL;
     size_t line_buf_size = 0;
-    int line_count = 0;
     ssize_t line_size;
     FILE *fp = fopen(FILENAME, "r");
     if (!fp)
@@ -19,19 +18,15 @@ int import_from_file(const char *FILENAME) {
 
     while (line_size >= 0)
     {
-        printf("%s\n", line_buf);
         import_contents(line_buf);
-        
-        /* Get the next line */
         line_size = getline(&line_buf, &line_buf_size, fp);
     }
 
-    /* Free the allocated line buffer */
     free(line_buf);
     line_buf = NULL;
 
-    /* Close the file now that we are done with it */
     fclose(fp);
+    return 0;
 }
 
 int main(int argc, char **argv) {
