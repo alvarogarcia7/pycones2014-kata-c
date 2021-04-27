d62e8d6ebbcaa8af6cacc436ded5da57fc9fc12d
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2021-04-27 20:34:46 +0400

    Saving contents

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+#include "lib.h"
 
-int main() {
+int main(int argc, char **argv) {
+    
+    import_contents()
+    if (argc >= 3 && strcmp(argv[2], "f") == 0 ){
+        follow_user(argv[1], argv[2]);
+    }
     printf("Hello, World!\n");
     return 0;
 }
